1179ea42d02362fa0cd0a4494cb7f790123aacdb
Mustafa GUNES <gunes149@gmail.com>
2023-12-02 18:51:06 +0300

    add app icon and remove storyboard

---
+++ baaas/baaas/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  baaas
+//
+//  Created by Mustafa Gunes on 2.12.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  
+  var window: UIWindow?
+  
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.backgroundColor = .white
+    window?.windowScene = windowScene
+    window?.rootViewController = UINavigationController(rootViewController: ViewController())
+    window?.makeKeyAndVisible()
+  }
+}
---
+++ baaas/baaas/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  baaas
+//
+//  Created by Mustafa Gunes on 2.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+  }
+}
